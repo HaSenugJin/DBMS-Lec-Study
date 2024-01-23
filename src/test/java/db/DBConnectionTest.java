af1216a37d3a2d4550867f082f7f2e3845cb79a0
egdg4587 <egdg7777@gmail.com>
2024-01-23 09:15:55 +0900

    셋팅2

--- src/test/java/db/DBConnectionTest.java
+++ src/test/java/db/DBConnectionTest.java
@@ -4,23 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnectionTest {
 
     // 리턴타입을 적을 수 없다.
     // 매개변수를 적을 수 없다.
-    // @Test 붙이면 메서드 별로 실행가능.
+    // @Test 붙이면 메서드 별로 실행가능
     @Test
     public void getInstance_test(){
-        String username = "root";
-        String password = "1234";
-        String url = "jdbc::mariadb::localhost::3306/";
+        // given = 파라미터
 
-        // 프로토콜이 적용된 소켓
-        try {
-            Connection conn = DriverManager.getConnection(url, username, password);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+
+        // when = 본코드 실행
+        Connection conn = DBConnection.getInstance();
+
+
+        // then = 눈 검증
+        if(conn == null){
+            System.out.println("실패");
+        }else{
+            System.out.println("성공");
         }
+
     }
 }
+
+
+
+
+
+
+
+
